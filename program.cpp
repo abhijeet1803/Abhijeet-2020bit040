34739ac87b8597ea6b38f72cbb03849183519ed9
abhijeet1803 <123958051+abhijeet1803@users.noreply.github.com>
2024-01-18 17:20:28 +0530

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,66 @@
+Name: ABHIJEET SINGH
+Name of the project: Blood management system (android app devleopment)
+Theme of the project with objective: to help and track blood donation camp in the remote area.
+
+public static void main(String args[]) {
+    string x = "Hello World";
+    System.out.println(string.reverse());
+}
+
+
+public static void main(String args[]) {
+    int arr = {1,2,3,4,5,6};
+    int arr2 = {};
+    for(int i=0;i<arr.length();i++) {
+        if(arr[i]%2==0) {
+            
+        }
+    }
+}
+
+int main() {
+    public static void main(String args[]) {
+        int arr() = {1,2,3,4,5,6,7,8,9};
+        int i,n;
+        int low=0,high=n,mid;
+        int mid = (low+high)/2;
+        for(i=0;i<n;i++) {
+            if(arr[mid]<arr[high]) {
+                mid=low+1;
+                return mid;
+            }
+            else 
+            {
+                if(arr[mid]>arr[low]) {
+                    mid=high-1;
+                    return mid;
+            }
+            else {
+                return mid;
+            }
+            }
+        }
+
+
+    }
+}
+
+int fact(n) {
+    int i,x;
+    for(i=0;i<n;i++) {
+        fact(0)=1;
+        if(n==0){
+            
+            else {
+                x=n*fact(n-1);
+            }
+        }
+    }
+    print(x);
+}
+
+void main() {
+    string words{} = ["flower","flow","flight"];
+    for(int i=0;i<words.length();i++)
+}
+
